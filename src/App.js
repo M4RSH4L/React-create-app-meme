b389127eebf5dc7d3f92d120529e82cb48321baf
Ronan <ronan.lavorato@gmail.com>
2021-09-30 18:04:27 -0300

    iniciando commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+var nombre = "Ronan"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          hola mundo soy {nombre}
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
