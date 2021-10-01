b2bcc6c55b1c42a7d5129624307d6a7dedb31312
Ronan <ronan.lavorato@gmail.com>
2021-09-30 22:47:27 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
-var nombre = "Ronan"
+
 function App() {
+
+  const [linea1, setLinea1] = useState ('');
+  const [linea2, setLinea2] = useState ('', '');
+
+ const onChangeline1 = function (valor) {
+   debugger;
+   alert (valor)
+   
+ }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hola mundo soy {nombre}
-          </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     
+      <select>
+         <option value="1">1</option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         <option value="5">5</option>
+         <option value="6">6</option>
+      </select> <br/>
+
+      <input onChange={onChangeline1}  type="Text" placeholder="linea1"/><br/>
+      <input type="Text" placeholder="linea2"/><br/>
+      <button>exportar</button>
+      
+      
+      <div>
+        <span>{linea1}</span> <br/>
+        <span>{linea2}</span> 
+        <img src=""></img>
+      </div>
+    
+  
     </div>
   );
-}
+} 
 
 export default App;
